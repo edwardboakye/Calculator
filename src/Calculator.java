7925887b9f6c741d502d1c003f389ea5b3e6a020
Edward Boakye <edwardboakye82@Gmail.com>
2019-01-19 12:50:02 -0500

    Add subtract and divide method

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,4 +7,9 @@ public class Calculator {
     public static double subtract(double num1, double num2){
         return num1-num2;
     }
+
+    public static double divide(double num1, double num2){
+        return num1/num2;
+    }
 }
+

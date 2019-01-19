7c26d43f4b9e0f36be30f58de2e0ec9a9d0460c6
edwardboakye <46761993+edwardboakye@users.noreply.github.com>
2019-01-19 16:39:47 -0500

    Change Added

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,6 +13,8 @@ public class Calculator {
     public static double divide(double num1, double num2){
         return num1/num2;
     }
-    
+   public static void change(){
+        system.out.println("Remote Changes");
+   } 
 }
 
